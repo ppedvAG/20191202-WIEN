3d2ad5655aeaf0366cd7bb83b8955fff540e1ac3
Vadzim Naumchyk <vadzimn@ppedv.de>
2019-12-05 15:49:19 +0100

    routing added

--- vadzim/myFirstNgApp/src/app/app-routing.module.ts
+++ vadzim/myFirstNgApp/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LabsModule } from './labs/labs.module';
+import { TodosModule } from './todos/todos.module';
+import { LabsOverviewComponent } from './labs/labs-overview/labs-overview.component'
+import { TodosOverviewComponent } from './todos/todos-overview/todos-overview.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'labs', component: LabsOverviewComponent},
+  {path: 'todos', component: TodosOverviewComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    LabsModule,
+    TodosModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
